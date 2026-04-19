b3658dbc084d85f6f500b3db198f9a85ce0d18ca
venkyroy22 <venkateswarakonetisetty@gmail.com>
2026-04-19 11:40:26 +0530

    fix: Completely remove binary field from muhammara package.json

--- patch-muhammara.js
+++ patch-muhammara.js
@@ -6,12 +6,12 @@ const pkgPath = path.join(__dirname, 'node_modules', 'muhammara', 'package.json'
 if (fs.existsSync(pkgPath)) {
   try {
     const pkg = JSON.parse(fs.readFileSync(pkgPath, 'utf8'));
-    if (pkg.binary && !pkg.binary.napi_versions) {
-      pkg.binary.napi_versions = [3];
+    if (pkg.binary) {
+      delete pkg.binary;
       fs.writeFileSync(pkgPath, JSON.stringify(pkg, null, 2));
-      console.log('Successfully patched muhammara/package.json with napi_versions to fix Turbopack build');
+      console.log('Successfully removed binary field from muhammara/package.json to fix Turbopack build');
     } else {
-      console.log('muhammara/package.json already has napi_versions or no binary field.');
+      console.log('muhammara/package.json already has no binary field.');
     }
   } catch (err) {
     console.error('Error patching muhammara:', err);
