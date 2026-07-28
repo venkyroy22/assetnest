8a7ebb8d250131cc8f1b7bba7d924df867d703d9
venkyroy22 <venkateswarakonetisetty@gmail.com>
2026-07-28 16:51:27 +0530

    chore(layout): add cpm monetization network script to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,6 +101,11 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5938543618083973"
           crossOrigin="anonymous"
         />
+        {/* Monetization Network Script */}
+        <script
+          async
+          src="https://pl30573991.effectivecpmnetwork.com/17/20/2b/17202bb25e2a84c0a9bf3eee7da992c9.js"
+        />
       </head>
 
       <body className="antialiased font-sans flex flex-col min-h-screen">
